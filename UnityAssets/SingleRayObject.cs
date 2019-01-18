33f45afad27d6f508e2d163ed4ce5c0689114f01
lukegehron <luke@gehron.com>
2019-01-18 17:01:47 -0500

    Update SingleRayObject.cs

--- UnityAssets/SingleRayObject.cs
+++ UnityAssets/SingleRayObject.cs
@@ -6,13 +6,13 @@ public class SingleRayObject : MonoBehaviour
 {
     public int lengthOfLineRenderer = 20;
 
-    public int counter = 0;
+    int counter = 0;
+
+    public Vector3[] points = new Vector3[20];
 
 
     Vector3 TravelVector = new Vector3(1, 0, 0);
-    Vector3[] points = new Vector3[20];
-
-
+    
 
 
     void Update()
@@ -23,7 +23,7 @@ public class SingleRayObject : MonoBehaviour
 
         
 
-        Vector3 OldPosition = points[19];
+        Vector3 OldPosition = points[lengthOfLineRenderer-1];
         Vector3 NewPosition = new Vector3(0, 0, 0);
 
 
@@ -49,19 +49,14 @@ public class SingleRayObject : MonoBehaviour
             Debug.Log("Did not Hit");
         }
 
-        //if raycast = true
-        //find raycast hit point and normal
-        //vector3.reflect using normal @ point
-        //change TravelVector to this new vector
-
-        // Change the NewPosition Vector's x and y components
+        
         NewPosition.x = OldPosition.x + TravelVector.x;
         NewPosition.y = OldPosition.y + TravelVector.y;
         NewPosition.z = OldPosition.z + TravelVector.z;
 
 
 
-        points[19] = NewPosition;
+        points[lengthOfLineRenderer-1] = NewPosition;
         lineRenderer.SetPositions(points);
         counter++;
 
