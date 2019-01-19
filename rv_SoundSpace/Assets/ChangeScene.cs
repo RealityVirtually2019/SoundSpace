84fe2dd9b24647f3096e41b2be55ac50b42d66c6
Acher249 <jchernick2010@gmail.com>
2019-01-19 17:18:50 -0500

    splash update
    splash update

---
+++ rv_SoundSpace/Assets/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject SplashVideo;
+
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("MR_MediaLab", LoadSceneMode.Single);
+        SplashVideo.SetActive(false);
+    }
+}
