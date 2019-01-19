fed633cb7cd3b57abf199c3185cea54bec6eee9d
cwmorse <cwm83@cornell.edu>
2019-01-19 11:51:33 -0500

    GetNRC

---
+++ rv_SoundSpace/Assets/Scripts/GetNRC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Autodesk.Forge.ARKit;
+using SimpleJSON;
+
+public static class GetNRC
+{
+    public static float getNRCFromCollider(Collider collider)
+    {
+
+        float result = 0.1f;
+        ForgeProperties fp = collider.gameObject.GetComponentInParent<ForgeProperties>();
+                
+        if (fp != null)
+        {
+            JSONNode temp = fp.Properties["props"];
+            Debug.Log("testing");
+            foreach (var v in temp.Values)
+            {
+                if (v["name"] == "Absorptance")
+                {
+                    string val = v["value"];
+                    float tryResult;
+                    if (float.TryParse(val, out tryResult))
+                        result = tryResult;
+                    
+                }
+            }
+        }
+
+        return result;
+    }
+}
