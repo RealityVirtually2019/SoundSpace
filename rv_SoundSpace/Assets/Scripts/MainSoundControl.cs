348192b3ff9d920dd5f97614ed509da76e5565b2
cwmorse <cwm83@cornell.edu>
2019-01-19 08:05:43 -0500

    Sound emit prefabs
    Moved raytrace1 to WaveEmitter script
    Created waveemitter prefab to instantiate and start from other scripts

---
+++ rv_SoundSpace/Assets/Scripts/MainSoundControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSoundControl : MonoBehaviour
+{
+
+    public GameObject emitterPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newEmitter = Instantiate(emitterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            newEmitter.GetComponent<WaveEmitter>().MakeNoise();
+        }
+    }
+}
