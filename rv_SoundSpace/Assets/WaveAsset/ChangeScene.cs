feb361a5f381178c53da4b3cb4f63f6eda2d4a4d
Adam Chernick <jchernick2010@gmail.com>
2019-01-20 11:21:53 -0500

    splash update
    splash update

--- rv_SoundSpace/Assets/WaveAsset/ChangeScene.cs
+++ rv_SoundSpace/Assets/WaveAsset/ChangeScene.cs
@@ -6,16 +6,23 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public GameObject SplashVideo;
+    public GameObject SplashVideo2;
 
     void Start()
     {
         StartCoroutine(Wait());
+        SplashVideo.SetActive(true);
+        SplashVideo2.SetActive(false);
     }
 
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(5);
-        SceneManager.LoadScene("MR_MediaLab", LoadSceneMode.Single);
+        yield return new WaitForSeconds(3.5f);
+
         SplashVideo.SetActive(false);
+        SplashVideo2.SetActive(true);
+
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene("MR_MediaLab", LoadSceneMode.Single);
     }
 }
