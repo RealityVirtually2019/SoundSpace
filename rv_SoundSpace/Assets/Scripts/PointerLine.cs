75c11d3b440e15bd3fc98a67ba75dcbdc316c794
cwmorse <cwm83@cornell.edu>
2019-01-20 07:53:38 -0500

    Show pointer for menu

---
+++ rv_SoundSpace/Assets/Scripts/PointerLine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.XR.WSA.Input;
+
+public class PointerLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var interactionSourceStates = InteractionManager.GetCurrentReading();
+        foreach (var interactionSourceState in interactionSourceStates)
+        {
+            if (interactionSourceState.source.handedness == InteractionSourceHandedness.Right)
+            {
+                Vector3 pos;
+                Vector3 forward;
+                Vector3 right;
+                Vector3 up;
+                if (interactionSourceState.sourcePose.TryGetPosition(out pos) && interactionSourceState.sourcePose.TryGetForward(out forward) && interactionSourceState.sourcePose.TryGetRight(out right) && interactionSourceState.sourcePose.TryGetUp(out up))
+                {
+                    forward = Quaternion.AngleAxis(35, right) * forward;
+                    pos = pos + (0.035f * up);
+                    RaycastHit hit;
+                    Ray ray = new Ray(pos, forward);
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        LineRenderer line = GetComponent<LineRenderer>();
+
+                        if (line != null)
+                        {
+                            line.enabled = true;
+                            line.SetPosition(0, pos);
+                            line.SetPosition(1, hit.point);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
