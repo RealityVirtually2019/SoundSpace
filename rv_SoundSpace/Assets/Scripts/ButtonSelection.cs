6fbcc3c31ab3faf2537556fae767bde780467256
Acher249 <jchernick2010@gmail.com>
2019-01-19 19:56:40 -0500

    button Selection
    button Selection

---
+++ rv_SoundSpace/Assets/Scripts/ButtonSelection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSelection : MonoBehaviour
+{
+    private Color32 NotSelectedColor = new Color32(106, 106, 106, 255);
+    private Color32 SelectedColor = new Color32(255, 0, 255, 255);
+
+    public void OnClick()
+    {
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log(name);
+    }
+}
