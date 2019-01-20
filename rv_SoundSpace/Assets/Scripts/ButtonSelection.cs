a44448617b025f01ad300b55cfdc8726a9588015
cwmorse <cwm83@cornell.edu>
2019-01-19 22:45:13 -0500

    Fixed beam position and button script

--- rv_SoundSpace/Assets/Scripts/ButtonSelection.cs
+++ rv_SoundSpace/Assets/Scripts/ButtonSelection.cs
@@ -11,25 +11,27 @@ public class ButtonSelection : MonoBehaviour
     public void OnClick()
     {
         var ClickedButton = EventSystem.current.currentSelectedGameObject;
-        var ClickedButtonisSelected;
+        Transform ClickedButtonisSelected;
 
         if(ClickedButton.transform.childCount > 0)
         {
             ClickedButtonisSelected = ClickedButton.transform.GetChild(0);
+
+            var sisterIsSelected = SisterButton.transform.GetChild(0);
+
+            //turn on or off isSelected panel of the button that you clicked on
+            if (ClickedButtonisSelected.gameObject.activeInHierarchy)
+            {
+                ClickedButtonisSelected.gameObject.SetActive(false);
+                sisterIsSelected.gameObject.SetActive(true);
+            }
+            else
+            {
+                ClickedButtonisSelected.gameObject.SetActive(true);
+                sisterIsSelected.gameObject.SetActive(false);
+            }
         }
 
-        var sisterIsSelected = SisterButton.transform.GetChild(0);
-
-        //turn on or off isSelected panel of the button that you clicked on
-        if (ClickedButtonisSelected.gameObject.activeInHierarchy)
-        {
-            ClickedButtonisSelected.gameObject.SetActive(false);
-            sisterIsSelected.gameObject.SetActive(true);
-        }
-           else
-        {
-            ClickedButtonisSelected.gameObject.SetActive(true);
-            sisterIsSelected.gameObject.SetActive(false);
-        }
+       
     }
 }
